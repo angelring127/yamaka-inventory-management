1ca9396ae46a46092e02820a31f66cc2bf46efde
hoya <angelring127@gmail.com>
2020-02-18 17:20:24 +0900

    Add : add softdelete in Record, StockManagement

--- app/StockManagement.php
+++ app/StockManagement.php
@@ -3,8 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 
 class StockManagement extends Model
 {
-  protected $fillable = ['big_category_id','item_id','middle_category_id','stock_status','stock_count','record_id'];
+  use SoftDeletes;
+
+  protected $fillable = ['big_category_id','item_id','middle_category_id','stock_status','stock_count','record_id','shipment_id','currentstock_count'];
 }
