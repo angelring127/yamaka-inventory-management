6820a9720b779ef5a54faae3274a0006cc8799cb
hoya <angelring127@gmail.com>
2020-02-18 21:25:39 +0900

    Add : 追加

--- resources/js/components/CalendarModal.js
+++ resources/js/components/CalendarModal.js
@@ -7,26 +7,39 @@ const CalendarModal = ({show, handleClose, stocks}) => {
   let initialMonth = new Date();
   let stockDates = [];
 
+  const [showShipmentView, setShipmentView] = useState(false);
+  const handleShipmentView = () => setShipmentView(!showShipmentView);
+
   // 在庫で出荷した日を表示
   if(typeof stocks !== 'undefined' && stocks.length > 0) {
-    stockDates = stocks.filter(stock => stock.stock_status === 1).map(stock => {
+    stockDates = stocks.filter(stock => stock.stock_status === 2).map(stock => {
       const created = new Date(stock.created_at);
       initialMonth = (created < initialMonth) ? created : initialMonth;
       return created;
     });
   }
-  console.log(stockDates);
-  console.log(initialMonth);
+
+  const dayPicker = <DayPicker numberOfMonths={3} 
+                      initialMonth={initialMonth}
+                      selectedDays={stockDates}
+                    />;
+  const shipmentView = <div>shipmentView</div>;
+
+  const modalTitle = !showShipmentView ? "製造カレンダー" : "出荷リスト";
+  const toggleBtnText = showShipmentView ? "製造カレンダー" : "出荷リスト";
+
   return (
     <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>出荷カレンダー</Modal.Title>
+          <Modal.Title>{modalTitle}</Modal.Title>
         </Modal.Header>
-        <Modal.Body><Row className="show-grid"><DayPicker numberOfMonths={3} 
-          initialMonth={initialMonth}
-          selectedDays={stockDates}
-        /></Row></Modal.Body>
+        <Modal.Body><Row className="show-grid">
+          { showShipmentView ? shipmentView : dayPicker}
+        </Row></Modal.Body>
         <Modal.Footer>
+          <Button variant="info" onClick={handleShipmentView}>
+          {toggleBtnText}
+          </Button>
           <Button variant="secondary" onClick={handleClose}>
             終了
           </Button>
